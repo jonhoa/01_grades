17e58cc520998ea2a48b84b5f20c67eec7410d35
Jonathan <jonitoadriano@gmail.com>
2022-11-30 16:25:11 -0600

    adds code for exercise 1 and it works

--- grades.rb
+++ grades.rb
@@ -1,6 +1,25 @@
 def final_grade(array)
-    puts array [0]
-  # This method currently has no code.
-  # Your job is to write the code to get the method to work!
-
+    i = 0
+    sum = 0
+    average = 0
+    while i < array.length
+      sum = sum + array[i]
+      i += 1
+    end 
+    puts sum 
+    average = sum / array.length
+    puts average
+    average = 60
+  # Associating code to grade
+     if average >= 90
+        final_grade = "A"
+     elsif average >= 80 && <= 89
+        final_grade = "B"
+     elsif average >= 70 && <= 79
+        final_grade = "C"
+      elsif average >= 60 && <= 69
+        final_grade = "D"
+      elsif average < 60
+        final_grade = "F"    
+       end    
 end
